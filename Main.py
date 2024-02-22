35b45113de8e5f1c592dcefe8ab865912066a849
Connor Harris <CWHarris0812@gmail.com>
2024-02-21 22:00:06 -0600

    Created todo list

---
+++ Main.py
@@ -0,0 +1,32 @@
+
+
+
+# TODO LIST
+#
+# CRITICAL
+# Load level from file
+# Display level
+# Add player
+# Add player movement
+# Add gravity
+# Add jumping
+#
+# HIGH
+# Add end of level
+# Add obstacles
+# Create an actual level
+#
+# MEDIUM
+# Add enemies
+# Add multiple levels
+# Add level selector
+#
+# LOW
+# Add start screen
+# Add graphics
+# Add sound
+#
+# ADDITIONAL FEATURES
+# Level creator
+# Import/export level
+# 2 player mode
