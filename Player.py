b6a8714d4d089c8060bfe2ea25cfbdd838826a4c
Connor Harris <CWHarris0812@gmail.com>
2024-02-24 23:09:03 -0600

    Player can no longer jump up through ground

--- Player.py
+++ Player.py
@@ -2,7 +2,7 @@ import pygame
 import math
 
 class Player():
-    SPEED = 100
+    SPEED = 150
     GRAVITY = 0.05
     JUMP_RATE = 3
     level_left, level_top = 0, 0
@@ -25,12 +25,21 @@ class Player():
         elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]: self.dx = 1
         else: self.dx = 0
 
-        if keys[pygame.K_UP] and self.isOnGround(board): 
+        #If up arrow and not already in air
+        if keys[pygame.K_UP] and not self.canMoveDown(board): 
             self.dy = -1 * self.JUMP_RATE
             self.yPos -= 1
 
-        #Update dy using gravity
-        if not self.isOnGround(board): self.dy += self.GRAVITY
+        #Stop going up if would hit an object
+        self.yPos += (self.dy * self.SPEED / self.FPS)
+        if not self.canMoveUp(board):
+            self.yPos -= (self.dy * self.SPEED / self.FPS)
+            self.dy = 0
+        else:
+            self.yPos -= (self.dy * self.SPEED / self.FPS)
+
+        #Update dy using gravity    
+        if self.canMoveDown(board): self.dy += self.GRAVITY
         else: self.dy = 0
 
         #Move
@@ -41,8 +50,8 @@ class Player():
     def getTopLeftPosition(self):
         return (self.xPos, self.yPos)
     
-    #Check if player is on the ground for jumping purposes
-    def isOnGround(self, board):
+    #Check if player can move down
+    def canMoveDown(self, board):
         #Calculate bottom and right sides of player
         playerBottom = self.yPos + self.scale_factor
         playerRight = self.xPos + self.scale_factor
@@ -56,6 +65,24 @@ class Player():
         for i in range(playerLeftGrid, playerRightGrid + 1):
             if board[playerBottomGrid + 1][i] == "#":
                 self.yPos = (playerBottomGrid - 1) * self.scale_factor + self.level_top
-                return True
+                return False
+            
+        return True
+    
+    #Check if player can move up
+    def canMoveUp(self, board):
+        #Calculate right side of player
+        playerRight = self.xPos + self.scale_factor
 
-        return False
+        #Convert player coordinates to grid
+        playerTopGrid = math.floor((self.yPos - self.level_top) / self.scale_factor)
+        playerLeftGrid = math.floor((self.xPos - self.level_left) / self.scale_factor)
+        playerRightGrid = math.ceil((playerRight - self.level_left) / self.scale_factor)
+
+        #Determine if these are touching the board
+        for i in range(playerLeftGrid, playerRightGrid + 1):
+            if board[playerTopGrid][i] == "#":
+                self.yPos = (playerTopGrid + 1) * self.scale_factor + self.level_top
+                return False
+            
+        return True
