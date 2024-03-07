8659830bf9e4b56fcd030fea61d2b4f9d74e6170
Connor Harris <CWHarris0812@gmail.com>
2024-03-06 22:55:12 -0600

    improved movement

--- Player.py
+++ Player.py
@@ -35,12 +35,10 @@ class Player():
             self.yPos -= 1
 
         #Stop going up if would hit an object
-        self.yPos += (self.dy * self.SPEED / self.FPS)
-        if not self.canMoveUp(board):
-            self.yPos -= (self.dy * self.SPEED / self.FPS)
+        topOpen, playerTopGrid = self.canMoveUp(board)
+        if not topOpen:
             self.dy = 0
-        else:
-            self.yPos -= (self.dy * self.SPEED / self.FPS)
+            self.yPos = (playerTopGrid + 1) * self.scale_factor + self.level_top
 
         #Stop going left if would hit an object
         leftOpen, playerLeftGrid = self.canMoveLeft(board)
@@ -48,15 +46,11 @@ class Player():
             self.dx = max(0, self.dx)
             self.xPos = (playerLeftGrid + 1) * self.scale_factor + self.level_left
 
-        #self.xPos = (playerLeftGrid + 1) * self.scale_factor + self.level_left
-
         #Stop going right if would hit an object
-        self.xPos += (self.dx * self.SPEED / self.FPS)
-        if not self.canMoveRight(board):
-            self.xPos -= (self.dx * self.SPEED / self.FPS)
+        rightOpen, playerRightGrid = self.canMoveRight(board)
+        if not rightOpen:
             self.dx = min(0, self.dx)
-        else:
-            self.xPos -= (self.dx * self.SPEED / self.FPS)
+            self.xPos = (playerRightGrid - 2) * self.scale_factor + self.level_left
 
         #Update dy using gravity    
         if self.canMoveDown(board): self.dy += self.GRAVITY / self.FPS
@@ -95,17 +89,16 @@ class Player():
         playerRight = self.xPos + self.scale_factor
 
         #Convert player coordinates to grid
-        playerTopGrid = math.floor((self.yPos - self.level_top) / self.scale_factor)
+        playerTopGrid = math.floor((self.yPos+ (self.dy * self.SPEED / self.FPS) - self.level_top) / self.scale_factor)
         playerLeftGrid = math.floor((self.xPos - self.level_left) / self.scale_factor)
         playerRightGrid = math.ceil((playerRight - self.level_left) / self.scale_factor)
 
         #Determine if these are touching the board
         for i in range(playerLeftGrid, playerRightGrid + 1):
             if board[playerTopGrid][i] == "#":
-                self.yPos = (playerTopGrid + 1) * self.scale_factor + self.level_top
-                return False
+                return False, playerTopGrid
             
-        return True
+        return True, playerTopGrid
     
     #Check if player can move left
     def canMoveLeft(self, board):
@@ -128,7 +121,7 @@ class Player():
     def canMoveRight(self, board):
         #Calculate bottom of player
         playerBottom = self.yPos + self.scale_factor
-        playerRight = self.xPos + self.scale_factor
+        playerRight = self.xPos + self.scale_factor + (self.dx * self.SPEED / self.FPS)
 
         #Convert player coordinates to grid
         playerTopGrid = math.floor((self.yPos - self.level_top) / self.scale_factor)
@@ -138,10 +131,9 @@ class Player():
         #Determine if these are touching the board
         for i in range(playerTopGrid, playerBottomGrid + 1):
             if board[i][playerRightGrid] == "#":
-                self.xPos = (playerRightGrid - 2) * self.scale_factor + self.level_left
-                return False
+                return False, playerRightGrid
             
-        return True
+        return True, playerRightGrid
     
     #Check if player has reached the end of the level
     def atLevelEnd(self, board):
